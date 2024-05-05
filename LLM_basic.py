8295df19432c531e6a3b5c985ea086a1592d80bd
Wrecklash <biradar.abhilash@gmail.com>
2024-05-05 14:56:52 +0530

    Testing

---
+++ LLM_basic.py
@@ -0,0 +1,6 @@
+from transformers import AutoModelForSequenceClassification
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+outputs = model(**inputs)
+print(outputs.logits.shape)
